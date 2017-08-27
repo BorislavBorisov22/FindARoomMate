afc48a33ab7b4cf6dd6c053b53672a10ca4a6521
Borislav Borisov <borislavborisov22@abv.bg>
2017-08-27 18:05:16 +0300

    Switched from material desing to bootstrap4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { CoreModule } from './core/core.module';
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent,
   ],
   imports: [
-    BrowserAnimationsModule,
+    BrowserModule,
     CoreModule.forRoot(),
     AppRoutingModule
   ],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,12 @@
 import { NgModule } from '@angular/core';
-import { MdButtonModule, MdCheckboxModule, MdInputModule, MdSidenav } from '@angular/material';
 import { HttpModule } from '@angular/http';
 
 @NgModule({
   imports: [
-    MdButtonModule,
-    MdCheckboxModule,
-    MdInputModule,
     HttpModule
   ],
   declarations: [],
   exports: [
-    MdButtonModule,
-    MdCheckboxModule,
-    MdInputModule,
     HttpModule
   ]
 })
