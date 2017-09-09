b942b213478ae79930e3e3865dbed3eb3d7f10ae
Borislav Borisov <borislavborisov22@abv.bg>
2017-09-09 04:03:28 +0300

    Added css loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,31 @@ import { NotificationService } from './services/notification.service';
 import { UsersService } from './services/users.service';
 import { Component, OnInit, ViewContainerRef, OnChanges, SimpleChanges } from '@angular/core';
 
+const LOADER_TIMEOUT = 2000;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnChanges {
+export class AppComponent implements OnInit {
+
+  public loaderHidden;
 
   constructor(private readonly notificationService: NotificationService,
     private readonly vcr: ViewContainerRef) {
     this.notificationService.init(vcr);
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log('do changes');
+  ngOnInit(): void {
+    this.loaderHidden = true;
+  }
+
+  onRouteChange() {
+    this.loaderHidden = false;
+    setTimeout(() => {
+      this.loaderHidden = true;
+    }, LOADER_TIMEOUT);
   }
 }
