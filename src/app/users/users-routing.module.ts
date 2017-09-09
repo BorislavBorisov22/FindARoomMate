7f2a3c7f1a6e54eda787402012ff50bf8bd380d6
Borislav Borisov <borislavborisov22@abv.bg>
2017-09-10 00:37:03 +0300

    Added users-all component

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,10 +1,12 @@
+import { UsersAllComponent } from './users-all/users-all.component';
 import { LogoutGuardService } from './../services/guards/logout-guard.service';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    { path: 'edit', component: EditProfileComponent, canActivate: [LogoutGuardService] }
+    { path: 'edit', component: EditProfileComponent, canActivate: [LogoutGuardService] },
+    { path: 'all', component: UsersAllComponent }
 ];
 
 @NgModule({
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -4,6 +4,7 @@ import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { UsersAllComponent } from './users-all/users-all.component';
 
 @NgModule({
   imports: [
@@ -12,6 +13,7 @@ import { EditProfileComponent } from './edit-profile/edit-profile.component';
   ],
   declarations: [
     EditProfileComponent,
+    UsersAllComponent,
   ]
 })
 export class UsersModule { }
