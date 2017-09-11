15a29187a9711c609f67e02c38c58b7fddf70e6d
Borislav Borisov <borislavborisov22@abv.bg>
2017-09-11 09:20:21 +0300

    Fixed navbar dropdown item

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    CONNECTION_STRING: 'mongodb://localhost/findaroommate',
+    CONNECTION_STRING: 'mongodb://dbuser:9603091722a@ds133044.mlab.com:33044/findaroommate',
     PORT: 4201,
 };
 
